c64f13dfbe2a36896c2942c83f742f3e7eed89c4
lykin06 <lykin06@gmail.com>
2014-01-22 19:18:20 +0100

    javadoc

--- src/Gestion/ListReservations.java
+++ src/Gestion/ListReservations.java
@@ -6,45 +6,63 @@ import Personnel.Borrower;
 import Serialization.Data;
 
 /**
- * Class ListReservations TODO commenter cette classe
+ * <b>Class ListReservations</b>
+ * <p>
+ * This class load and store the list of reservations from the reservation.data
+ * file.
+ * </p>
  * 
  * @author Aurelien COLOMBET
  * 
  */
 public class ListReservations {
+    /**
+     * List of reservations
+     */
     private ArrayList<Reservation> reservations;
 
+    /**
+     * <b>Constructor</b>
+     * <p>
+     * Generate the list of reservation from the file
+     * </p>
+     * 
+     * @see Data#load(String)
+     */
     public ListReservations() {
         // TODO voir si on peut enlever ce warning
-        this.setReservations((ArrayList<Reservation>) Data.load("reservation"));
+        this.reservations = (ArrayList<Reservation>) Data.load("reservation");
     }
 
     /**
+     * <b>Add a reservation</b>
+     * 
      * @param reservation
-     * @return a boolean which says if the reservation it is possible.
+     * @return a boolean which says if the reservation it is stored.
      */
     public boolean add(Reservation reservation) {
-        this.getReservations().add(reservation);
-        return Data.store(this.getReservations(), "reservation");
+        this.reservations.add(reservation);
+        return Data.store(this.reservations, "reservation");
     }
 
     /**
+     * <b>Remove a reservation</b>
      * 
      * @param reservation
      *            Method which remove a reservation from database.
      */
     public void remove(Reservation reservation) {
-        for (int i = 0; i < this.getReservations().size(); i++) {
-            if (this.getReservations().get(i).equals(reservation)) {
-                this.getReservations().remove(i);
+        for (int i = 0; i < this.reservations.size(); i++) {
+            if (this.reservations.get(i).equals(reservation)) {
+                this.reservations.remove(i);
                 break;
             }
         }
-        Data.store(this.getReservations(), "reservation");
+        Data.store(this.reservations, "reservation");
     }
 
     /**
-     * @return the reservations
+     * @return the reservations list
      */
     public ArrayList<Reservation> getReservations() {
         return reservations;
@@ -52,33 +70,49 @@ public class ListReservations {
 
     /**
      * @param reservations
-     *            the reservations to set
+     *            the reservations list to set
      */
     public void setReservations(ArrayList<Reservation> reservations) {
         this.reservations = reservations;
     }
 
+    /**
+     * <b>Return the reservations of the current user</b>
+     * 
+     * @param currentUser
+     * @return a list of reservation
+     */
     public ArrayList<Reservation> getReservationsOf(Borrower currentUser) {
-        ArrayList<Reservation> res = new ArrayList<>();
-        for (int i = 0; i < this.getReservations().size(); i++) {
-            if (this.getReservations().get(i).getBorrower().equals(currentUser)) {
-                res.add(this.getReservations().get(i));
+        ArrayList<Reservation> userReservations = new ArrayList<Reservation>();
+
+        for (int i = 0; i < this.reservations.size(); i++) {
+            if (this.reservations.get(i).getBorrower().equals(currentUser)) {
+                userReservations.add(this.reservations.get(i));
             }
         }
-        return res;
+
+        return userReservations;
     }
 
+    /**
+     * <b>authorize a reservation</b>
+     * 
+     * @param reservation to authorize
+     */
     public void authorize(Reservation reservation) {
-        for (int i = 0; i < this.getReservations().size(); i++) {
-            if (this.getReservations().get(i).equals(reservation)) {
-                this.getReservations().get(i).setAccepted(true);
+        for (int i = 0; i < this.reservations.size(); i++) {
+            if (this.reservations.get(i).equals(reservation)) {
+                this.reservations.get(i).setAccepted(true);
             }
         }
     }
 
+    /**
+     * <b>Reset the reservation file</b>
+     */
     public void reinitialize() {
-        this.setReservations(new ArrayList<Reservation>());
-        Data.store(this.getReservations(), "reservation");
+        this.reservations = new ArrayList<Reservation>();
+        Data.store(this.reservations, "reservation");
     }
 
 }
